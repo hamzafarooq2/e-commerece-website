f518842cc9bb93770ae9e34b2d5d03f3905050d4
AliRaza198 <alirazach322ali@gmail.com>
2023-04-19 15:57:58 +0500

    Add javaScript code for toolTip

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
-$('#mainCarousel').carousel({
-    interval: 9000
+// JavaScript for Carousel
+$("#mainCarousel").carousel({
+  interval: 9000,
 });
 
-let carousel=document.querySelectorAll('carousel1');
-for(let i=0;i<carousel.length;i++){
-    carousel[i].addEventListener('mouseout', function(e){carousel[i].pause()})
+let carousel = document.querySelectorAll("carousel1");
+for (let i = 0; i < carousel.length; i++) {
+  carousel[i].addEventListener("mouseover", function (e) {
+    carousel[i].pause();
+  });
 }
+
+// JavaScript for Tooltip in Carousels
+var setUpTooltip = function () {
+  let tooltip = "",
+    tooltipDiv = document.querySelector(".div-tooltip"),
+    tooltipElements = Array.from(document.querySelectorAll(".hover-reveal"));
+  var displayToolTip = function (e, obj) {
+    tooltip = obj.dataset.tooltip;
+    tooltipDiv.innerHTML = tooltip;
+    tooltipDiv.getElementsByClassName.top = e.pageY + "px";
+    tooltipDiv.getElementsByClassName.left = e.pageX + "px";
+    tooltipDiv.getElementsByClassName.opacity = 1;
+  };
+  tooltipElements.forEach(function(elem){
+    elem.addEventListener("mouseover", function(e){
+        displayToolTip(e, this);
+    });
+    elem.addEventListener("mouseleave", function(e){
+        tooltipDiv.getElementsByClassName.opacity = 0;
+    });
+  });
+};
