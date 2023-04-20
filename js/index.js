58633dc4c1fd3fcfc1644f07a47a1cbf417ed9c2
AliRaza198 <alirazach322ali@gmail.com>
2023-04-20 20:05:11 +0500

    add tooltip code in JQuery

--- js/index.js
+++ js/index.js
@@ -1,33 +1,7 @@
 // JavaScript for Carousel
-$("#mainCarousel").carousel({
-  interval: 9000,
+
+// code for tooltip
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
-let carousel = document.querySelectorAll("carousel1");
-for (let i = 0; i < carousel.length; i++) {
-  carousel[i].addEventListener("mouseover", function (e) {
-    carousel[i].pause();
-  });
-}
-
-// JavaScript for Tooltip in Carousels
-var setUpTooltip = function () {
-  let tooltip = "",
-    tooltipDiv = document.querySelector(".div-tooltip"),
-    tooltipElements = Array.from(document.querySelectorAll(".hover-reveal"));
-  var displayToolTip = function (e, obj) {
-    tooltip = obj.dataset.tooltip;
-    tooltipDiv.innerHTML = tooltip;
-    tooltipDiv.getElementsByClassName.top = e.pageY + "px";
-    tooltipDiv.getElementsByClassName.left = e.pageX + "px";
-    tooltipDiv.getElementsByClassName.opacity = 1;
-  };
-  tooltipElements.forEach(function(elem){
-    elem.addEventListener("mouseover", function(e){
-        displayToolTip(e, this);
-    });
-    elem.addEventListener("mouseleave", function(e){
-        tooltipDiv.getElementsByClassName.opacity = 0;
-    });
-  });
-};
