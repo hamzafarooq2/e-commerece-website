72cd38437b3ec5a27658711ad95232de2562c4d0
AliRaza198 <alirazach322ali@gmail.com>
2023-05-08 02:23:45 +0500

    uncomment the Mega menu code

--- js/index.js
+++ js/index.js
@@ -10,14 +10,22 @@
 //  var tooltipBottom = new bootstrap.Tooltip(carousel-item, { placement : "bottom" });
 
 // Mega Menu
-// function changeImg(imgchanger) {
-//   document.getElementById("slider").src = imgchanger;
-// }
+function changeImg(imgchanger) {
+  document.getElementById("slider").src = imgchanger;
+}
 
 // function show() {
-//   var image = document.getElementById('showImage').value;
+//   var image1 = document.getElementById('showImage1');
+//   var image2 = document.getElementById('showImage2');
+//   if(image1.src == image2.src){
+//     image2.src=image1.src;
+//     console.log("object");
+//   }
+//   else{
+//     console.log("object2");
+//   }
 
 // image Src="".something = secondPage ;
-// document.getElementById("demo").innerHTML=localStorage.something;
+//   document.getElementById("demo").innerHTML=localStorage.something;
 //   document.getElementById("product-main-image")= document.getElementById("showImage");
 // }
